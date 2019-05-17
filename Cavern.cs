f25be2fbd8c42d2d9ac86ffd54f5e66b5856a5dd
LucyLavend <jongebloedlucy@gmail.com>
2019-05-17 12:00:00 +0200

    initial commit

---
+++ Cavern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CavernCS
+{
+	class Cavern
+	{
+		/**
+		 * Create and play the Game.
+		 */
+		public static void Main (string[] args)
+		{
+			Game game = new Game();
+			game.play();
+		}
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavernCS
+{
+    public class Player
+    {
+        public Room currentRoom;
+
+        public Player()
+        {
+
+        }
+
+        
+    }
+}
