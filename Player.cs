2fa8436bd2437dbbf1875775551c33ab2606de6b
LucyLavend <jongebloedlucy@gmail.com>
2019-05-19 12:01:22 +0200

    update tacked files

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavernCS
+{
+    public class Player
+    {
+        public Room currentRoom;
+
+        public Player()
+        {
+
+        }
+
+        public Room getCurrentRoom()
+        {
+            return currentRoom;
+        }
+
+        public void setCurrentRoom(Room r)
+        {
+            currentRoom = r;
+        }
+    }
+}
