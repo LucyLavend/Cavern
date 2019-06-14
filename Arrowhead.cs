b591c7fee8a2b7bcb29ca81b27c87db99dab73c8
LucyLavend <jongebloedlucy@gmail.com>
2019-06-14 09:14:26 +0200

    add map and arrowhead item

---
+++ Arrowhead.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavernCS
+{
+    public class Arrowhead : Item
+    {
+
+        public Arrowhead()
+        {
+            this.name = "Arrowhead";
+        }
+
+        public Arrowhead(string name, string description, float weight) : base(name, description, weight)
+        {
+
+        }
+
+        public override void use(Object o)
+        {
+            if (o.GetType() == typeof(Player))
+            {
+                Player p = (Player)o; // must cast
+                p.damage(5);
+            }
+            else
+            {
+                // Object o is not a Person
+                System.Console.WriteLine("Can't use Object on " + o.GetType());
+            }
+        }
+    }
+}
---
+++ Map.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavernCS
+{
+    public class Map : Item
+    {
+
+        public Map()
+        {
+            this.name = "Map";
+        }
+
+        public Map(string name, string description, float weight) : base(name, description, weight)
+        {
+
+        }
+    }
+}
